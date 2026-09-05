f57d94cff774a43d96d3323641bb6476cd7b1401
yemen-plaster <ly4590087@gmail.com>
2026-09-05 19:44:57 +0300

    Initial production website

---
+++ app/sitemap.ts
@@ -0,0 +1,13 @@
+
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: "https://maqalah-market.netlify.app",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 1,
+        },
+    ];
+}
