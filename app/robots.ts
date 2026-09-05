c1c5941d023f5beceeb8d9470989c17633855f37
Ali | Software Engineer & AI Architect <ly4590087@gmail.com>
2026-09-06 01:45:35 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: "*",
             allow: "/",
         },
-        sitemap: "https://maqalah-market.netlify.app/sitemap.xml",
+        sitemap: "https://bahar-al-jumaili.vercel.app/sitemap.xml",
     };
 }
 
